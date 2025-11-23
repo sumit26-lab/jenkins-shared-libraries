d04575539020daa5d27f7e81c441dea7609fa3b5
sumit26-lab <59225172+sumit26-lab@users.noreply.github.com>
2025-11-23 19:59:50 +0530

    Correct docker build command and echo statements
    Fix syntax errors and improve echo statements in docker_build.groovy.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,11 @@
 def (String DockerHubUser,String ProjectName,String ImageTage){
       echo "image tage process Start"
-    sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTage}
+     echo "DockerHubUser = ${DockerHubUser}"
+     echo "ProjectName = ${ProjectName}"
+     echo "ImageTage = ${ImageTage}"
+      
+    sh """
+      docker build -t ${DockerHubUser}/${ProjectName}:${ImageTage} .
+      """
 
 }
