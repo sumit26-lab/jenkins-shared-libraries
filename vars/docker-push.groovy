b9153628bb04066c55647eb83b705ab54e845908
sumit26-lab <59225172+sumit26-lab@users.noreply.github.com>
2025-11-23 16:01:00 +0530

    Add docker-push.groovy for Docker image pushing

---
+++ vars/docker-push.groovy
@@ -0,0 +1,14 @@
+def call(String dockerhubUser, String ProjectName,ImageTage){
+                withCredentials([usernamePassword(credentialsId: 'dockerhubCred', 
+                usernameVariable: 'DOCKER_HUB_USER',
+                passwordVariable: 'DOCKER_HUB_PASS')]) {
+                
+                // loginDocker hub
+                sh "docker login -u ${env.DOCKER_HUB_USER} -p ${env.DOCKER_HUB_PASS}"
+                // create image tage
+          
+                sh "docker push ${env.DOCKER_HUB_USER}/${ProjectName}:${ImageTage}
+                echo "image on push docker hub"
+                
+                }
+}
